e040beb644050456f19e8359eb5f28548b233d45
abbelod <123talhaahmed@gmail.com>
2024-07-29 16:27:19 +0500

    uploaded dist directory for frontend production build

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ morgan.token('content', function getContent(req){
     return JSON.stringify(req.body)
 })
 
+
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time :content'))
