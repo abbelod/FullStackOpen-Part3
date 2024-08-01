b4214171e79cb072b5e8ca09c725fcbe163a0472
abbelod <123talhaahmed@gmail.com>
2024-08-01 13:56:16 +0500

    backend update and delete func

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ morgan.token('content', function getContent(req){
 })
 
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError'){
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+
+
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
@@ -76,19 +86,11 @@ app.get('/api/persons/:id', (request,response)=>{
 
 app.delete('/api/persons/:id', (request,response)=>{
 
-Person.findById(request.params.id).then(person=>{
-  if(person){
-
-    response.json(person)
-  }
-  else{
-    response.status(404).end()
-  }
-})
-.catch(error=>{
-  console.log(error)
-  response.status(400).send({error:'malformed id'})
-})
+  Person.findByIdAndDelete(request.params.id)
+  .then(result=> {
+    response.status(204).end()
+  })
+  .catch(error=>next(error))
 
 })
 
@@ -123,6 +125,25 @@ app.post('/api/persons', (request,response)=>{
     
 }
 )
+
+app.put('/api/persons/:id', (request,response,next) => {
+  const body = request.body
+
+  const person =  {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new:true})
+  .then(updatedPerson=>{
+    response.json(updatedPerson)
+  })
+  .catch(error=>next(error))
+})
+
+
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
